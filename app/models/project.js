6e8add6fd1d782ef196563eecfa835a6f06ce236
Karan Jeet <karanjeet96@gmail.com>
2015-12-06 00:13:10 +0530

    user Project schema

---
+++ app/models/project.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+	local: {
+		author_username: String,
+        author_name: String
+	},
+    name: String,
+    username: String,
+	members: [{ 
+        username: String,
+        name: String,
+        userpic: String,
+        userid: String,
+        time: { type: Date, default: Date.now }
+    }],  
+    troupes: [{ 
+        username: String,
+        name: String,
+        userpic: String,
+        userid: String,
+        members: [{ 
+            username: String,
+            name: String,
+            userpic: String,
+            userid: String,
+            time: { type: Date, default: Date.now }
+        }],
+        time: { type: Date, default: Date.now }
+    }],  
+	rank: { type: String, default: '0' },
+    tags: { type: String, default: '0' },
+    target: { type: String, default: 'public'},
+    privacy: { type: String, default: 'public'},
+    posts: { type: String, default: '0' },
+	meta: {
+		likes: String,
+	    followers: { type: String, default: '0' },
+        share: { type: String, default: '0' },
+		comment: [{
+            username: String,
+            message: String,
+            time: String
+        }]
+	},
+	created_at: { type: Date, default: Date.now },
+	updated_at: { type: Date, default: '' }
+});
+
+//module.exports = mongoose.model('User', userSchema);
+
+var Project = mongoose.model('project', userSchema);
+module.exports = Project;
