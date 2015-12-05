e4bed592994db360ab4aeff39c459619f611a0cb
Karan Jeet <karanjeet96@gmail.com>
2015-12-05 20:00:33 +0530

    adding project route '/api/project'

--- app/routes/api.js
+++ app/routes/api.js
@@ -6,6 +6,10 @@ var Notify = require('../models/notify');
 var Addrsbook = require('../models/addrsbook');
 
 module.exports = function(router){
+    
+    /*------------------------------------------------------
+    ------------------------ TROUPE ------------------------
+    ------------------------------------------------------*/
     router.post('/troupe', function(req, res){
         console.log(req.body);
         console.log(req.user.local.username);
@@ -74,6 +78,76 @@ module.exports = function(router){
     });
     
     /*------------------------------------------------------
+    ------------------------ PROJECTS ----------------------
+    ------------------------------------------------------*/
+    router.post('/project', function(req, res){
+        console.log(req.body);
+        console.log(req.user.local.username);
+        var troupe = new Troupe();
+        troupe.name = req.body.troupename;
+        troupe.username = req.body.username;
+        troupe.local.author_username = req.user.local.username;
+        /*if(req.body.address) {
+            troupe.address.street = req.body.address.street;
+            troupe.address.city = req.body.address.city;
+            troupe.address.state = req.body.address.state;
+            troupe.address.zip = req.body.address.zip;
+        }*/
+        
+        troupe.save(function(err, data){
+            if(err)
+                throw err;
+            res.json(data);
+        });
+    });
+    
+    router.get('/project', function(req, res){
+        Troupe.find({'local.author_username': req.user.local.username}, function(err, data){
+            res.json(data);
+        });
+    });
+    
+    router.delete('/project', function(req, res){
+        Troupe.remove({}, function(err){
+            res.json({result: err ? 'error' : 'ok'});
+        });
+    });
+    
+    router.get('/project/:id', function(req, res){
+        Troupe.findOne({_id: req.params.id}, function(err, data){
+            res.json(data);
+        })
+    })
+    
+    router.delete('/project/:id', function(req, res){
+        Troupe.remove({_id: req.params.id}, function(err){
+            res.json({result: err ? 'error' : 'ok'});
+        })
+    })
+    
+    router.post('/project/:id', function(req, res){
+        Troupe.findOne({_id: req.params.id}, function(err, data){
+            var troupe = data;
+            troupe.name = req.body.troupename;
+            troupe.username = req.body.username;
+            troupe.local.author_username = req.user.local.username;
+            /*if(req.body.address) {
+                troupe.address.street = req.body.address.street;
+                troupe.address.city = req.body.address.city;
+                troupe.address.state = req.body.address.state;
+                troupe.address.zip = req.body.address.zip;
+            }*/
+            
+            troupe.save(function(err, data){
+                if(err)
+                    throw err;
+                res.json(data);
+            });
+            
+        });
+    });
+    
+    /*------------------------------------------------------
     ------------------------- POSTS ------------------------
     ------------------------------------------------------*/
     router.post('/posts', function(req, res){
@@ -97,7 +171,7 @@ module.exports = function(router){
     });
     
     router.get('/posts', function(req, res){
-        Posts.find({'local.author_username': req.user.local.username}, function(err, data){
+        Posts.find({}, function(err, data){
             res.json(data);
         });
     });
