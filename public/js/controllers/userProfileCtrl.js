e8446323286b4b67010b1f5b57b6a2922bbdb914
Karan Jeet <karanjeet96@gmail.com>
2015-12-06 22:38:13 +0530

    user Profile Controller

--- public/js/controllers/userProfileCtrl.js
+++ public/js/controllers/userProfileCtrl.js
@@ -1,17 +1,23 @@
 myApp.controller('userProfileCtrl', ['$scope', '$routeParams', 'Api', function($scope, $routeParams, Api){
+ $scope.tabs = [
+    { title:'Messages', content:'Dynamic content 2' },
+    { title:'Test', content:'Dynamic content 3' }
+  ];
+    $scope.currentuser = {};
+    $scope.cuser = {};
     $scope.userName = $routeParams.userName;
-    $scope.userProfile = {};
     
-    $scope.tabs = [
-        { title:'Messages', content:'Dynamic content 2' },
-        { title:'Test', content:'Dynamic content 3' }
-      ];
-    
-    Api.User.get({id: $scope.userName }, function(data){
-        $scope.userProfile = data;
-        console.log('User (data)');
-        console.log(data);
-        console.log($scope.userProfile);
-        console.log($scope.userProfile.fname);
+  Api.User.get({id: $scope.userName }, function(data){
+        $scope.currentuser = data;
+        console.log('Active User (data)');
+        console.log($scope.currentuser);
     });
+    
+    
+    $scope.updateinfo = function(){
+        Api.User.save({id: $scope.userName }, $scope.cuser,
+        function(){
+            console.log('updated');
+        });
+    }
 }]);
