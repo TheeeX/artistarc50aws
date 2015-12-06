5d2727ce9a217cbba2713ab1f6a011442ee79d37
Karan Jeet <karanjeet96@gmail.com>
2015-12-06 18:55:01 +0530

    'Activeuser' to 'User'

--- public/js/services/Api.js
+++ public/js/services/Api.js
@@ -1,18 +1,28 @@
 myApp.factory('Api',['$resource', function($resource){
     return {
-        Troupe: $resource('/api/troupe/:id', {id: '@id'}),
-        Project: $resource('/api/project/:id', {id: '@id'}),
-        Posts: $resource('/api/posts/:id', {id: '@id'}),
-        Likes: $resource('/api/likes/:id', {id: '@id'}),
-        Artist: $resource('/api/artist/:id', {id: '@id'}),
-        SearchObj: $resource('/api/search/:target/:id', {target: '@target', id: '@id'},
-            {
-                'save':  {method:'POST', isArray:true},
-            }
-        ),
-        ActiveUser: $resource('/api/activeuser/:id', {id: '@id'}),
-        UserTroupeOne: $resource('/api/user/troupe/:id', {id: '@id'}),
-        UserAddrsbook: $resource('/api/user/addbook/:id', {id: '@id'}),
-        Notify: $resource('/api/notify/:type/:sid/:tid/:notf', {type: '@type', sid: '@sid', tid: '@tid', notf: '@notf'})
+        Troupe:
+            $resource('/api/troupe/:id', {id: '@id'}),
+        UserTroupeOne: 
+            $resource('/api/user/troupe/:id', {id: '@id'}),
+        Project: 
+            $resource('/api/project/:id', {id: '@id'}),
+        Posts: 
+            $resource('/api/posts/:id', {id: '@id'}),
+        Likes: 
+            $resource('/api/likes/:id', {id: '@id'}),
+        Artist: 
+            $resource('/api/artist/:id', {id: '@id'}),
+        SearchObj: 
+            $resource('/api/search/:target/:id', {target: '@target', id: '@id'},
+                {
+                    'save':  {method:'POST', isArray:true},
+                }
+            ),
+        User: 
+            $resource('/api/user/:id', {id: '@id'}),
+        UserAddrsbook: 
+            $resource('/api/user/addbook/:id', {id: '@id'}),
+        Notify: 
+            $resource('/api/notify/:type/:sid/:tid/:notf', {type: '@type', sid: '@sid', tid: '@tid', notf: '@notf'})
     }
 }])
