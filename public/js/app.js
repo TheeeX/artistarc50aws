d3b0195ab244ea3c7ae3a6e7f50d42810c41fb02
Karan Jeet <karanjeet96@gmail.com>
2015-12-06 22:37:33 +0530

    userProfile route {combining user and search userProfile}

--- public/js/app.js
+++ public/js/app.js
@@ -8,18 +8,18 @@ var myApp = angular.module('myApp', [
     'chart.js']).
 	config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
 		$routeProvider.when('/page1', {templateUrl: 'partials/page1.ejs', controller: 'page1Controller'});
+        
 		$routeProvider.when('/page2', {templateUrl: 'partials/page2.ejs'});
         
         $routeProvider.when('/test', {templateUrl: 'partials/test.ejs', controller: 'testController'});
         
-        $routeProvider.when('/user', {templateUrl: 'partials/user.ejs', controller: 'userController'});
+        $routeProvider.when('/user/:userName', {templateUrl: 'partials/user.ejs', controller: 'userProfileCtrl'});
+        
         $routeProvider.when('/edit', {templateUrl: 'partials/edit.ejs', controller: 'aboutController'});
+        
         $routeProvider.when('/analytics', {templateUrl: 'partials/analytics.ejs', controller: 'analyticsController'});
         
-        
         $routeProvider.when('/troupe/:userName', {templateUrl: '../partials/troupe.ejs', controller: 'troupeProfileCtrl'});
-        
-        $routeProvider.when('/user/:userName', {templateUrl: '../partials/user.ejs', controller: 'userProfileCtrl'});
         
 		$routeProvider.otherwise({redirectTo: '/test'});
 
