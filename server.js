ba94b7dbe1ba6f6aac09c8105448d81ffb2a3833
Karan Jeet <karanjeet96@gmail.com>
2015-12-11 05:29:44 +0530

    aws sdk integration and bucket querry from s3

--- server.js
+++ server.js
@@ -15,7 +15,19 @@ var multer = require('multer'); // v1.0.5
 var upload = multer();                         // for parsing multipart/form-data
 
 var AWS = require('aws-sdk');
-AWS.config.region = 'us-west-2';
+AWS.config.loadFromPath('./config/aws.json');
+//AWS.config.region = 'us-west-2';
+
+var s3 = new AWS.S3();
+s3.listBuckets(function(err, data) {
+  if (err) { console.log("Error:", err); }
+  else {
+    for (var index in data.Buckets) {
+      var bucket = data.Buckets[index];
+      console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+    }
+  }
+});
 
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
